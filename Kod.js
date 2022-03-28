091a22a65c3459a910b592aa068cff5787ce699a
Albin.Bergh.nti.johanneberg <92986696+GruvGurr@users.noreply.github.com>
2022-03-28 19:46:07 +0200

    synk för arbete hemma

--- Kod.js
+++ Kod.js
@@ -1,1 +1,13 @@
 <script onclick="alert"></script>
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var div = document.createElement('div');
+    div.id = 'container';
+    div.innerHTML = 'Hi there!';
+    div.className = 'border pad';
+ 
+    document.body.appendChild(div);
+}, false);
+
+var row = document.createElement('DIV') 
